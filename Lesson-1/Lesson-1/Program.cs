e5f4bf7212c0ab86564deeb30016a8da6407a4e3
Сергей Авдеев <65767554+Besmozka@users.noreply.github.com>
2021-05-04 16:39:35 +0500

    Add project for first lesson

---
+++ Lesson-1/Lesson-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        
+        private static readonly string _sourceAddress = "https://jsonplaceholder.typicode.com/posts/";
+
+        private static readonly string _responseFile = "Responses.txt";
+
+        private static int _firstPostIndex = 4;
+
+        private static int _postsListLenght = 10;
+
+
+        static async Task Main(string[] args)
+        {
+            HttpResponseMessage response = await _client.GetAsync(_sourceAddress + _firstPostIndex);                                               
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            
+            var s = JsonSerializer.Deserialize<ResponseModel>(responseBody, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            }); 
+
+            Console.WriteLine();
+        }
+    }  
+}
---
+++ Lesson-1/Lesson-1/ResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    public class ResponseModel
+    {
+        public int UserId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public ResponseModel(int userId, int id, string title, string body)
+        {
+            UserId = userId;
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+    }
+}
