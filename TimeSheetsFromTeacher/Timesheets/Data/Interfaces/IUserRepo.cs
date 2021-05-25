b432615dc6e872cc07efe7d5e86b4743bd769210
SergeyAvdeev <avdey87@mail.ru>
2021-05-25 21:40:57 +0500

    Add CRUD to User

---
+++ TimeSheetsFromTeacher/Timesheets/Data/Interfaces/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Timesheets.Models;
+
+namespace Timesheets.Data.Interfaces
+{
+    public interface IUserRepo
+    {
+        Task<User> GetByLoginAndPasswordHash(string login, byte[] passwordHash);
+        Task<User> GetById(Guid id);
+        Task CreateUser(User user);
+        Task DeleteUser(Guid id);
+    }
+}
